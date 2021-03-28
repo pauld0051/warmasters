441dd86223ae913f8947b9da2d4e84a6d71f821d
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-03-28 10:31:40 +0200

    Add checkout success logic and template

--- checkout/models.py
+++ checkout/models.py
@@ -30,20 +30,17 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-    #def update_total(self):
+    def update_total(self):
         """
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        #self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
-            #'lineitem_total__sum']
-        #if self.order_total < settings.FREE_GIFT_THRESHOLD:
-            #self.delivery_cost = self.order_total * \
-                #settings.STANDARD_DELIVERY_PERCENTAGE / 100
-        #else:
-            #self.delivery_cost = 0
-        #self.grand_total = self.order_total + self.delivery_cost
-        #self.save()
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
+            'lineitem_total__sum'] or 0
+        self.delivery_cost = 0
+        # Keeping a delivery cost in as a just in case measure for future projects
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
 
     def save(self, *args, **kwargs):
         """
