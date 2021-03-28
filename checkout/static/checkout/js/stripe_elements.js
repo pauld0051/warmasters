02c74cf3a3ebecb6c36cf3aec00edcec3938d1f1
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-03-28 11:50:22 +0200

    Add loading overlay for checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -35,8 +35,7 @@ card.addEventListener("change", function (event) {
             <span class="icon text-lime" role="alert">
                 <i class="fas fa-times"></i>
             </span>
-            <span class="text-lime">${event.error.message}</span>
-        `;
+            <span class="text-lime">${event.error.message}</span>`;
     $(errorDiv).html(html);
   } else {
     errorDiv.textContent = "";
@@ -44,33 +43,35 @@ card.addEventListener("change", function (event) {
 });
 
 // Handle form submit
-var form = document.getElementById("payment-form");
+var form = document.getElementById('payment-form');
 
-form.addEventListener("submit", function (ev) {
-  ev.preventDefault();
-  card.update({ disabled: true });
-  $("#submit-button").attr("disabled", true);
-  stripe
-    .confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card,
-      },
-    })
-    .then(function (result) {
-      if (result.error) {
-        var errorDiv = document.getElementById("card-errors");
-        var html = `
-                <span class="icon text-lime" role="alert">
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
-                <span class="text-lime">${result.error.message}</span>`;
-        $(errorDiv).html(html);
-        card.update({ disabled: false });
-        $("#submit-button").attr("disabled", false);
-      } else {
-        if (result.paymentIntent.status === "succeeded") {
-          form.submit();
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
         }
-      }
     });
 });
