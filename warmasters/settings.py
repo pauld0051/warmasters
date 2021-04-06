d96b0ecb7ebd0c50e0f1e9756dbaa69b269dcb83
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-04-06 12:03:54 +0200

    Fix incorrect  email address

--- warmasters/settings.py
+++ warmasters/settings.py
@@ -221,7 +221,7 @@ DEFAULT_FROM_EMAIL = 'warmasters616@gmail.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = warmasters616@gmail.com'
+    DEFAULT_FROM_EMAIL = 'warmasters616@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
