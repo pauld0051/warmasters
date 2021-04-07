07d26d575156da9ad1d846d7728c5e5be88e4b99
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-04-07 08:55:19 +0200

    Add defensive programming to prevent accidental delete of products

--- products/forms.py
+++ products/forms.py
@@ -16,7 +16,10 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        self.fields['rating'].widget.attrs['id'] = 'spinners'
+
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0 bkg-white text-dk-gray'
+            
